9fae13a8547fdc4c8ccc6fb1ddfdaabc43237831
jaellekondohoma <jaellekondohoma@10.43.16.202>
2019-11-13 15:16:24 -0600

    palindrome runs until i want to exit

--- src/unl/cse/recursion/Palindrome.java
+++ src/unl/cse/recursion/Palindrome.java
@@ -10,22 +10,31 @@ public class Palindrome {
 			return true;
 		}
 
-		if(s.charAt(0) == s.charAt(s.length() - 1)) {
+		if (s.charAt(0) == s.charAt(s.length() - 1)) {
 			return isPalindrome(s.substring(1, s.length() - 1));
-			
-			
+
 		}
-		
+
 		return false;
 	}
 
 	public static void main(String args[]) {
 
+		boolean exit = false;
+
 		Scanner s = new Scanner(System.in);
 		Palindrome p = new Palindrome();
-		System.out.print("Enter a word: ");
-		String str = s.next();
-		System.out.println(str + " is a palindrome? " + p.isPalindrome(str));
+		while (!exit) {
+			System.out.print("Enter a word: ");
+			String str = s.next();
+			System.out.println(str + " is a palindrome? " + p.isPalindrome(str));
+			System.out.println();
+			if (str.equalsIgnoreCase("exit")) {
+				System.out.println("exiting.....");
+				exit = true;
+			}
+
+		}
 		s.close();
 	}
 }
