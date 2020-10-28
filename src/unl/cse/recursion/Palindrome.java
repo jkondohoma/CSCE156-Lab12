aeefd1541410b3eee6a1121d288d10f6019d6e8e
jkondohoma <36901483+jkondohoma@users.noreply.github.com>
2020-10-28 13:54:48 -0500

    Update Palindrome.java

--- src/unl/cse/recursion/Palindrome.java
+++ src/unl/cse/recursion/Palindrome.java
@@ -5,36 +5,16 @@ import java.util.Scanner;
 public class Palindrome {
 
 	public boolean isPalindrome(String s) {
-
-		if (s.length() == 1 || s.isEmpty()) {
-			return true;
-		}
-
-		if (s.charAt(0) == s.charAt(s.length() - 1)) {
-			return isPalindrome(s.substring(1, s.length() - 1));
-
-		}
-
+		// TODO: you need to implement this method
 		return false;
 	}
 
 	public static void main(String args[]) {
 
-		boolean exit = false;
-
 		Scanner s = new Scanner(System.in);
 		Palindrome p = new Palindrome();
-		while (!exit) {
-			System.out.print("Enter a word: ");
-			String str = s.next();
-			System.out.println(str + " is a palindrome? " + p.isPalindrome(str));
-			System.out.println();
-			if (str.equalsIgnoreCase("exit")) {
-				System.out.println("exiting.....");
-				exit = true;
-			}
-
-		}
-		s.close();
+		System.out.println("Enter a word: ");
+		String str = s.next();
+		System.out.println(str + " is a palindrome? " + p.isPalindrome(str));
 	}
 }
